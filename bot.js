0e3f6672f33a019a93b6242a2e32ce0dd383caa2
wqeqwpqaduhqe97512 <41009951+wqeqwpqaduhqe97512@users.noreply.github.com>
2018-07-22 23:13:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,30 +1,223 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-client.on('message', message => {
-    if (message.content.startsWith("رابط")) {
-        message.channel.createInvite({
-        thing: true,
-        maxUses: 1,
-        maxAge: 86400,
-    }).then(invite =>
-      message.author.sendMessage(invite.url)
-    )
-    const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-          .setDescription(" تم أرسال الرابط برسالة خاصة ")
-           .setAuthor(client.user.username, client.user.avatarURL)
-                 .setAuthor(client.user.username, client.user.avatarURL)
-                .setFooter('طلب بواسطة: ' + message.author.tag)
-
-      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
-              const Embed11 = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        
-    .setDescription(" مدة الرابط : يوم  عدد استخدامات الرابط : 1 ")
-      message.author.sendEmbed(Embed11)
-    }
-}); 
+const ytdl = require('ytdl-core');
+const request = require('request');
+const fs = require('fs');
+const getYoutubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
+const http2 = require('http2');
 
 
-client.login('NDM5MTQwNjk1MDAxNzkyNTEy.DcO_ng.7hLxSETvPJVvelEGA7uGpgQBVEM');
+const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
+const prefix = '_';
+const discord_token = "NDY1OTIwODkyOTk2ODEyODEw.DjZYkA.CPDKI6Jnr3sc4N7UZINY5Kz9-00";
+client.login(discord_token);
+client.on('ready', function() {
+ console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+  console.log('By : @! HuNteR');
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+	
+});
+
+var servers = ['351519476879196174','351519138566373386','','','','','','','','','','','','',''];
+var queue = [];
+var guilds = ['',''];
+var queueNames = [];
+var isPlaying = false;
+var dispatcher = null;
+var voiceChannel = null;
+var skipReq = 0;
+var skippers = [];
+var now_playing = [];
+
+client.on('ready',  () => {
+
+client.user.setGame(`_play`,'https://www.twitch.tv/hix')
+client.user.setStatus("online");
+});
+
+
+
+client.on('ready', () => {});
+var download = function(uri, filename, callback) {
+	request.head(uri, function(err, res, body) {
+		console.log('content-type:', res.headers['content-type']);
+		console.log('content-length:', res.headers['content-length']);
+
+		request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+	});
+};
+
+client.on('message', function(message) {
+	const member = message.member;
+	const mess = message.content.toLowerCase();
+	const args = message.content.split(' ').slice(1).join(' ');
+
+	if (mess.startsWith(prefix + 'play')) {
+		if (!message.member.voiceChannel) return message.reply('** انت لست في روم الاغاني **');
+		// if user is not insert the URL or song title
+		if (args.length == 0) {
+			let play_info = new Discord.RichEmbed()
+				.setAuthor(client.user.username, client.user.avatarURL)
+				.setDescription('**هل يمكنك وضع اسم اغنيه او رابط اغنيه**')
+			message.channel.sendEmbed(play_info)
+			return;
+		}
+		if (queue.length > 0 || isPlaying) {
+			getID(args, function(id) {
+				add_to_queue(id);
+				fetchVideoInfo(id, function(err, videoInfo) {
+					if (err) throw new Error(err);
+					let play_info = new Discord.RichEmbed()
+						.setAuthor("تم وضعها في قائمه ", message.author.avatarURL)
+						.setDescription(`**${videoInfo.title}**`)
+						.setColor("RANDOM")
+						.setFooter('تم تشغيل بواسطه:' + message.author.tag)
+						.setImage(videoInfo.thumbnailUrl)
+					//.setDescription('?')
+					message.channel.sendEmbed(play_info);
+					queueNames.push(videoInfo.title);
+					// let now_playing = videoInfo.title;
+					now_playing.push(videoInfo.title);
+
+				});
+			});
+		}
+	else if (mess.startsWith(prefix + 'skip')) {
+		if (!message.member.voiceChannel) return message.channel.send(':no_entry: || **Must BE at a voice channel**');
+		message.channel.send(':ok:').then(() => {
+			skip_song(message);
+			var server = server = servers[message.guild.id];
+			if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+		});
+	}
+	else if (message.content.startsWith(prefix + 'vol')) {
+		if (!message.member.voiceChannel) return message.channel.send(':no_entry: || **Must BE at a voice channel**');
+		// console.log(args)
+		if (args > 100) return message.channel.send('1 - 100')
+		if (args < 1) return message.channel.send('1 - 100')
+		dispatcher.setVolume(1 * args / 50);
+		message.channel.sendMessage(`** ${dispatcher.volume*50}%  volume **`);
+	}
+	else if (mess.startsWith(prefix + 'stop')) {
+		if (!message.member.voiceChannel) return message.channel.send(':no_entry: || **Must BE at a voice channel**');
+		message.channel.send(':ok:').then(() => {
+			dispatcher.pause();
+		});
+	}
+	else if (mess.startsWith(prefix + 'ruseme')) {
+		if (!message.member.voiceChannel) return message.channel.send(':no_entry: || ** Must BE at a voice channel**');
+			message.channel.send(':ok:').then(() => {
+			dispatcher.resume();
+		});
+	}
+	else if (mess.startsWith(prefix + 'go')) {
+		if (!message.member.voiceChannel) return message.channel.send(':no_entry: || **Must BE at a voice channel**');
+		message.channel.send(':ok:');
+		var server = server = servers[message.guild.id];
+		if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+	}
+	else if (mess.startsWith(prefix + 'join')) {
+		if (!message.member.voiceChannel) return message.channel.send(':no_entry: || ** Must BE at a voice channel  **');
+		message.member.voiceChannel.join().then(message.channel.send(':ok:'));
+	}
+	else if (mess.startsWith(prefix + 'play')) {
+		if (!message.member.voiceChannel) return message.channel.send(':no_entry: || **Must BE at a voice channel**');
+		if (isPlaying == false) return message.channel.send(':anger: || **Stoped**');
+		let playing_now_info = new Discord.RichEmbed()
+			.setAuthor(client.user.username, client.user.avatarURL)
+			.addField('Playing:', `**
+				  ${videoInfo.title}
+				  **`)
+			.setColor("RANDOM")
+			.setFooter('request By : ' + message.author.tag)
+			.setThumbnail(videoInfo.thumbnailUrl)
+		//.setDescription('?')
+		message.channel.sendEmbed(playing_now_info);
+	}
+});
+
+function skip_song(message) {
+	if (!message.member.voiceChannel) return message.channel.send(':no_entry: || **Must BE at a voice channel**');
+	dispatcher.end();
+}
+
+function playMusic(id, message) {
+	voiceChannel = message.member.voiceChannel;
+
+
+	voiceChannel.join().then(function(connectoin) {
+		let stream = ytdl('https://www.youtube.com/watch?v=' + id, {
+			filter: 'audioonly'
+		});
+		skipReq = 0;
+		skippers = [];
+
+		dispatcher = connectoin.playStream(stream);
+		dispatcher.on('end', function() {
+			skipReq = 0;
+			skippers = [];
+			queue.shift();
+			queueNames.shift();
+			if (queue.length === 0) {
+				queue = [];
+				queueNames = [];
+				isPlaying = false;
+			}
+			else {
+				setTimeout(function() {
+					playMusic(queue[0], message);
+				}, 500);
+			}
+		});
+	});
+}
+
+function getID(str, cb) {
+	if (isYoutube(str)) {
+		cb(getYoutubeID(str));
+	}
+	else {
+		search_video(str, function(id) {
+			cb(id);
+		});
+	}
+}
+
+function add_to_queue(strID) {
+	if (isYoutube(strID)) {
+		queue.push(getYoutubeID(strID));
+	}
+	else {
+		queue.push(strID);
+	}
+}
+
+function search_video(query, cb) {
+    request("https://www.googleapis.com/youtube/v3/search?part=id&type=video&q=" + encodeURIComponent(query) + "&key=" + yt_api_key, function(error, response, body) {
+        try {
+        var json = JSON.parse(body);
+        cb(json.items[0].id.videoId);
+        } catch (e) {
+    cb('EgqUJOudrcM');
+    return;
+
+    console.error(e);
+            
+        }
+    });
+}
+
+
+
+function isYoutube(str) {
+	return str.toLowerCase().indexOf('youtube.com') > -1;
+}
+
+
+
+
+
+
